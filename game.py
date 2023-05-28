4f9f1f793dc86b0a8a3c37b2b4d9414bd953143e
goshatopxxx <134185195+goshatopxxx@users.noreply.github.com>
2023-05-28 09:48:03 +0500

    Update game.py

--- game.py
+++ game.py
@@ -24,15 +24,91 @@ class Player(Sprite):
     ):
         super().__init__(image, x, y, width, height)
         self.speed = speed
+        self.k_up = k_up
+        self.k_down = k_down
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[self.k_up] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[self.k_down] and self.rect.y < (
+            WINDOW_HEIGHT - self.rect.height
+        ):
+            self.rect.y += self.speed
+
+class Ball(Sprite):
+    dx = 5
+    dy = 5
+
+    def update(self, player_1, player_2):
+        if self.rect.y <= 0:
+            self.dy *= -1
+        if self.rect.y >= (WINDOW_HEIGHT - self.rect.height):
+            self.dy *= -1
+        if self.rect.colliderect(player_1.rect):
+            self.dx *= -1
+        if self.rect.colliderect(player_2.rect):
+            self.dx *= -1
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+
+    def is_outside(self):
+        if self.rect.x < -self.rect.width:
+            return 'left'
+        elif self.rect.x > WINDOW_WIDTH:
+            return 'right'
+        else:
+            return 'none'
 
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Пинг Понг')
 window.fill(BG_COLOR)
 clock = pygame.time.Clock()
 
+player_left = Player(
+    ROCKET_IMG, 5, 5, 30, 100, 9, pygame.K_w, pygame.K_s
+)
+player_right = Player(
+    ROCKET_IMG, WINDOW_WIDTH - 35, WINDOW_HEIGHT - 105,
+    30, 100, 9, pygame.K_UP, pygame.K_DOWN
+)
+ball = Ball(BALL_IMG, 40, 40, 32, 32)
+
 game_status = 'game'
 while game_status != 'off':
     window.fill(BG_COLOR)
+
+    if game_status == 'game':
+        ball_status = ball.is_outside()
+        if ball_status == 'left':
+            game_status = 'result'
+            font = pygame.font.SysFont('Arial', 40)
+            text = font.render('Победил 2 игрок', True, (200, 200, 200))
+        elif ball_status == 'right':
+            game_status = 'result'
+            font = pygame.font.SysFont('Arial', 40)
+            text = font.render('Победил 1 игрок', True, (200, 200, 200))
+        ball.update(player_left, player_right)
+        player_left.update()
+        player_right.update()
+        player_left.draw()
+        player_right.draw()
+        ball.draw()
+    elif game_status == 'result':
+        rect = text.get_rect()
+        rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+        window.blit(text, (rect.x, rect.y))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_r]:
+            game_status = 'game'
+            player_left = Player(
+                ROCKET_IMG, 5, 5, 30, 100, 9, pygame.K_w, pygame.K_s
+            )
+            player_right = Player(
+                ROCKET_IMG, WINDOW_WIDTH - 35, WINDOW_HEIGHT - 105,
+                30, 100, 9, pygame.K_UP, pygame.K_DOWN
+            )
+            ball = Ball(BALL_IMG, 40, 40, 32, 32)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,5 +116,3 @@ while game_status != 'off':
 
     clock.tick(60)
     pygame.display.update()
-
-    
